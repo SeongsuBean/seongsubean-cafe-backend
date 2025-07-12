ea01fdc71d7da78b50b70b5801aea21cb7946524
goldtwo1989 <ttpd19891213@gmail.com>
2025-07-12 17:08:36 +0900

    style: 카페 vo 네이밍 수정(액터+기능)

--- src/main/java/com/oopsw/seongsubeancafebackend/vo/RequestOwnerCreateCafe.java
+++ src/main/java/com/oopsw/seongsubeancafebackend/vo/RequestOwnerCreateCafe.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class RequestCafe {
+public class RequestOwnerCreateCafe {
   private String cafeName;
   private String businessLicense;
   private String zipCode;
